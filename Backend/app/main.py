73b689095e93de353c27fe94dcf00ef7d07f7253
Trisha Barma <trishabarma@Trishas-MacBook-Air.local>
2026-07-19 20:03:22 +0530

    add cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from routes import root, trip_routes, weather_routes, place_routes
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="AI Trip Planner API")
+
+
+origins = [
+    "https://aitripplanner-omega.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(root.router)
 app.include_router(trip_routes.router)
